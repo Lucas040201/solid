235cf6aebcb164dcc8a2f7be937a9f7394179fbf
Lucas Mendes <lucas.mendes@4linux.com.br>
2024-01-02 20:56:21 -0300

    feat: create user observable

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Customer;
+use App\Models\Shopkeeper;
+use App\Observers\UserObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -25,7 +28,8 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Customer::observe(UserObserver::class);
+        Shopkeeper::observe(UserObserver::class);
     }
 
     /**
