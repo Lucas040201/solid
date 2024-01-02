2e2d32dbe027e076da9a5fb55d61105043671130
Lucas Mendes <lucas.mendes@4linux.com.br>
2024-01-02 20:59:07 -0300

    feat: create user controller

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Exceptions\ProviderNotFoundException;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateUserRequest;
+use App\Services\UserService;
+use Mockery\Exception;
+
+class UserController extends Controller
+{
+
+    public function __construct(
+        private readonly UserService $userService
+    )
+    {
+    }
+
+    public function create(CreateUserRequest $request, string $provider)
+    {
+        try {
+            $data = $request->validated();
+
+            $user = $this->userService->createUser($data, $provider);
+
+            return response([
+                'message' => 'User created Successfully',
+                'id' => $user->uuid->toString()
+            ]);
+
+        } catch (ProviderNotFoundException|Exception $exception) {
+            return response([
+                'error' => [
+                    'message' => $exception->getMessage()
+                ]
+            ], $exception->getCode());
+        }
+    }
+
+    public function index()
+    {
+        try {
+            return response([
+                'data' => $this->userService->index()
+            ]);
+        } catch (Exception $exception) {
+            return response([
+                'error' => [
+                    'message' => $exception->getMessage()
+                ]
+            ], $exception->getCode());
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('user')->group(function() {
+   Route::controller(UserController::class)->group(function() {
+       Route::post('/{provider}', 'create');
+   });
+});
