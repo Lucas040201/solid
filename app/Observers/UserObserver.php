68cb8557c112a2d70dba99c43160966ed6869cd5
Lucas Mendes <lucas.mendes@4linux.com.br>
2024-01-11 22:05:02 -0300

    feat: creating wallet when insert a user

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Contracts\UserContract;
+use App\Models\Wallet;
 use Ramsey\Uuid\Uuid;
 
 class UserObserver
@@ -10,5 +11,7 @@ class UserObserver
     public function creating(UserContract $user)
     {
         $user->uuid = Uuid::uuid4();
+
+        Wallet::create(['user_id' => $user->uuid]);
     }
 }
